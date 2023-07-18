beb020593746cc546b0375f3d6794016bf10692d
Daniel Martinek <martid12@fit.cvut.cz>
2023-07-18 18:09:23 +0200

    Small cleanup

--- src/components/ItemCard.js
+++ src/components/ItemCard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import Stack from 'react-bootstrap/Stack';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
--- src/pages/BlogPage.js
+++ src/pages/BlogPage.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import MainLayout from '../layouts/MainLayout';
 import PostPreview from '../components/PostPreview';
 import Stack from 'react-bootstrap/Stack';
